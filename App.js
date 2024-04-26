68b71dec457c8b8a24b157aaef239106cf0fb418
Rujutha <168082937+rujutha-t-p@users.noreply.github.com>
2024-04-26 22:45:10 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,10 +1,30 @@
-import LoginForm from './Components/LoginForm/LoginForm';
+import React from 'react';
+import './LoginForm.css';
 
 function App() {
   return (
-    <div>
-      <LoginForm/>
-    </div>
+    <div className='wrapper'>
+            <form action="">
+                <h1>Login</h1>
+                <div className="input-box">
+                    <input type="text" placeholder='Username'required/>
+                   
+                </div>
+                <div className="input-box">
+                    <input type="password" placeholder='Password'required/>
+                    
+                </div>
+                <div className="remember-forgot">
+                    <label><input type="checkbox"/>Remember me</label>
+                    <a href="#">Forgot password?</a>
+                </div>
+                <button type="submit">Login</button>
+                <div className="register-link">
+                    <p>Don't have an account? <a href="#">Register</a></p>
+                </div>
+
+            </form>
+        </div>
   );
 }
 
